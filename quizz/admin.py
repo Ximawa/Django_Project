5472aae65268cf3abcfebf588dd841df24e33544
Sebastien Fernet <JohnDoe@example.com>
2022-04-12 14:20:56 +0200

    Ajout methode str au models.py et MAJ admin

--- quizz/admin.py
+++ quizz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Categorie, Question, Reponse
+
 # Register your models here.
+
+admin.site.register(Categorie)
+admin.site.register(Question)
+admin.site.register(Reponse)
