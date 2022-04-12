b7893c7b29936f4baeda97a80c4ea762c9087008
Sebastien Fernet <JohnDoe@example.com>
2022-04-12 18:48:23 +0200

    Creation quizz

--- quizz/forms.py
+++ quizz/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from quizz.models import Categorie
+
 class SignInForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(max_length=30)
@@ -16,8 +18,19 @@ class AuthenticateForm(forms.Form):
     password = forms.CharField(max_length=30)
 
     def clean(self):
-        cleaned_data = super(SignInForm, self).clean()
+        cleaned_data = super(AuthenticateForm, self).clean()
         username = cleaned_data.get('username')
         password = cleaned_data.get('password')
         if not username and not password:
+            raise forms.ValidationError('Vous devez rentrer vos informations')
+
+class QuizzCreationForm(forms.Form):
+    nom = forms.CharField(max_length=200)
+    categorie = forms.ModelChoiceField(queryset=Categorie.objects.all().order_by('nom'))
+
+    def clean(self):
+        cleaned_data = super(QuizzCreationForm, self).clean()
+        nom = cleaned_data.get('nom')
+        categorie = cleaned_data.get('categorie')
+        if not nom and not categorie:
             raise forms.ValidationError('Vous devez rentrer vos informations')
--- quizz/urls.py
+++ quizz/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
     path('Signin', views.signNewUser, name="signNewUser"),
     path('Login', views.loginuser, name="loginuser"),
     path('Welcome', views.Welcomepage, name="Welcomepage"),
-    path('Logout', views.logoutpage, name="logoutpage")
+    path('Logout', views.logoutpage, name="logoutpage"),
+    path('QuizzList', views.Quizzlistpage, name="Quizzlistpage"),
+    path('Quizzcreationpage', views.Quizzcreationpage, name="Quizzcreationpage")
 ]
